ae05f24d046e96e9a98d8b2f7ed778546fc185b6
Glitch (coordinated-face) <none>
2019-01-22 19:34:20 +0000

    🐋🍍 Updated with Glitch

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors')
 
 const app = express();
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(cors())
 
 var quoteSchema = new mongoose.Schema({
   quote: {type: String, required: true},
+  quoteId: Number,
   author: {type: String, required: true},
+  category: String,
   fccCamper: String,
   camper_link: String,
   date: Date
-})
+}, {capped: {size: 64, max: 100}});
+
 
 var quoteCounterSchema = new mongoose.Schema({
   name: String,
@@ -25,14 +30,15 @@ var quoteCounterSchema = new mongoose.Schema({
 var Quote = mongoose.model("Quote", quoteSchema);
 var Counter = mongoose.model("Counter", quoteCounterSchema);
 
-function addToDb(record, callback) {
-  console.log('addtodb ', record)
+function addToDb(record, id, callback) {  
   var newQuote = new Quote({
     quote: record.quote,
+    quoteId: id,
     author: record.author,
+    category: record.category,
     fccCamper: record.camper,
     camper_link: record.camper_link,
-    date: new Date()
+    date_submitted: new Date()
   })
   newQuote.save(function(err, result) {
     if (err) console.error(err);    
@@ -42,8 +48,7 @@ function addToDb(record, callback) {
 
 function checkIfExists(collection, searchJson, callback) {
   collection.find(searchJson, function(err, results) {
-    if (err) console.error(err);
-    console.log(results.length)
+    if (err) console.error(err);    
     if (results.length == 0) {
       callback(false)
     } else {
@@ -59,6 +64,47 @@ function createDbCounter() {
   })
 }
 
+function incrementCounter(callback) {
+  Counter.findOne({name: "quoteId"}, function(err, result) {
+    if (err) console.error(err);    
+    result.counter += 1;
+    result.save(function(err, result) {
+      if (err) console.error(err);
+      callback(result.counter)
+    })
+  })
+}
+
+function getQuoteNumber(callback) {
+  Counter.findOne({name: "quoteId"}, function(err, result) {
+    if (err) console.error(err);    
+    callback(result)
+  })  
+}
+
+function getQuote(searchJson, callback) {
+  Quote.find(searchJson, {_id: 0, quoteId: 0, __v: 0}, function(err, results) {
+    if (err) console.error(err);
+    if (results.length == 0) {
+      callback({})
+      //callback({quote: "No such category", author: "No such category"})
+    } else {
+      callback(results[Math.floor(Math.random()*results.length)])
+    }
+  })  
+}
+
+function getAllQuotes(callback) {
+  Quote.find({}, {_id: 0, quoteId: 0, __v: 0}, function(err, results) {
+    if (err) console.error(err);
+    if (results.length == 0) {
+      callback({})      
+    } else {
+      callback(results)
+    }
+  })  
+}
+
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html')
   checkIfExists(Counter, {name: "quoteId"}, function(result) {
@@ -69,15 +115,44 @@ app.get('/', function(request, response) {
 });
 
 app.post('/api/addquote', function(req, res) {  
-  addToDb(req.body, function() {            
-    res.json(req.body)
+  incrementCounter(function(counter) {
+    addToDb(req.body, counter, function() {            
+    res.redirect('/')
+    })  
   })
 });
 
-/*app.get('/api/quote', function(req, res) {
-  getQuote();
+app.get('/api/quotenum', function(req, res) {
+  getQuoteNumber(function(result) {
+    res.json(result)
+  });  
+})
+
+app.get('/api/quote/all', function(req, res) {
+  getQuote({}, function(result) {
+    res.json(result)
+  })
+});
+
+app.get('/api/quote/allquotes', function(req, res) {
+  getAllQuotes(function(results) {
+    res.json(results)
+  })
+});
+
+app.get('/api/quote/other', function(req, res) {
+  getQuote({}, function(result) {
+    res.json(result)
+  })
+});
+
+app.get('/api/quote/:cat', function(req, res) {    
+  getQuote({category:req.params.cat}, function(result) {
+    res.json(result)
+  })
+});
   
-})*/
+  
 
 
 const listener = app.listen(process.env.PORT, function() {
